054e1014e88c9b45d1256e4018b16934497996eb
Iara Assis <iaraassisnega@gmail.com>
2023-08-30 19:10:12 -0300

    feat:search page

--- src/pages/SearchPage/index.jsx
+++ src/pages/SearchPage/index.jsx
@@ -1,18 +1,34 @@
 import {MdSearch} from "react-icons/md";
+import styles from "./style.module.scss";
+import { useState } from "react";
 
 export const SearchPage = () =>{
+    const [searchFiltered, setSearchFiltered] = useState(false);
+
     return(
-        <main>
-            <h1>Busca de vagas</h1>
-            <p>Digite o que você está procurando:</p>
-            <form>
-                <input type="text" placeholder="Pesquisa"/>
-                <button className="" type="submit">
-                    <MdSearch size={25}/>
-                </button>
-            </form>
+        <main className="container">
+            <div className={styles.searchContainer}>
+                <h1 className="title one">Busca de vagas</h1>
+                <p className="title boldAlert ">Digite o que você está procurando:</p>
+                <form>
+                    <input type="text" placeholder="Pesquisa"/>
+                    <button className="" type="submit">
+                        <MdSearch size={25}/>
+                    </button>
+                </form>
+            <div className={styles.filter}>
+                <h2 className="paragraph strong">Resultados de busca para: <strong className="title boldAlert">desenvolvedor</strong></h2>
+                <h3 className="title two ">Desculpe :(!</h3>
+                <p className="paragraph">Nenhun resultado encontrado</p>
+
+                <ul>
+
+                </ul>
+            </div>
+            </div>
         </main>
     )
 }
+
 
 
