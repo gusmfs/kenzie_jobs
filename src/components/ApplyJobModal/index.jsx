9a89b9ef553a3734024f10c5706991652c000d01
Rhayanny rodrigues <rhayannyrodrigues71 @gmail.com>
2023-08-30 18:08:36 -0300

    fix:ApplyModal

--- src/components/ApplyJobModal/index.jsx
+++ src/components/ApplyJobModal/index.jsx
@@ -2,7 +2,6 @@ import style from "./style.module.scss";
 import { AiOutlineClose } from "react-icons/ai";
 
 export const ApplyJobModal = () => {
-  
   return (
     <aside className={style.modalOverlay} role="dialog">
       <form>
