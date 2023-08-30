4052bfa0d7821283144994a981910b8e97606d88
Alana Maia <alanaalvesmaia@gmail.com>
2023-08-29 23:50:48 -0300

    feat: create JobsListSection

---
+++ src/components/sections/JobsListSection/index.jsx
@@ -0,0 +1,17 @@
+export const JobsListSection = () => {
+  return (
+    <>
+      <section>
+        <div className="container sm">
+          <div>
+            <h2>Confira nossas vagas</h2>
+            <ul>
+              {/* criar um componente JobsList */}
+              <li>{/* criar um componente JobsListCard */}</li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
