9b96f02574df5ac682a49243f08ca7c03a573b28
Alana Maia <alanaalvesmaia@gmail.com>
2023-08-31 16:21:00 -0300

    refactor: link LoginPage to RegisterPage

--- src/pages/RegisterPage/index.jsx
+++ src/pages/RegisterPage/index.jsx
@@ -4,23 +4,33 @@ import { TextArea } from "../../components/TextArea";
 import { DefaultTemplate } from "../DefaultTemplate";
 import { AiOutlinePlusCircle } from "react-icons/ai";
 import styles from "./style.module.scss";
+import { BiArrowBack } from "react-icons/bi";
+import { Link } from "react-router-dom";
 
 export const RegisterPage = () => {
   return (
     <>
       <DefaultTemplate>
-        <main className="container xs">
+        <main className="container">
           <div className={styles.registerBox}>
-            <h2 className="title one blue center">Cadastre-se</h2>
-            <Form>
-              <Input type="text" placeholder="Nome da empresa" />
-              <Input type="email" placeholder="E-mail" />
-              <Input type="password" placeholder="Senha" />
-              <Input type="password" placeholder="Confirmar senha" />
-              <button className={`${styles.buttonCreate} btnSolid`}>
-                <span>Cadastrar-se</span>
-              </button>
-            </Form>
+            <Link to={"/login"} className={styles.link}>
+              <BiArrowBack />
+              <span class="paragraph bold">voltar</span>
+            </Link>
+            <div className="container xs">
+              <div className={styles.registerForm}>
+                <h2 className="title one blue center">Cadastre-se</h2>
+                <Form>
+                  <Input type="text" placeholder="Nome da empresa" />
+                  <Input type="email" placeholder="E-mail" />
+                  <Input type="password" placeholder="Senha" />
+                  <Input type="password" placeholder="Confirmar senha" />
+                  <button className={`${styles.buttonCreate} btnSolid`}>
+                    <span>Cadastrar-se</span>
+                  </button>
+                </Form>
+              </div>
+            </div>
           </div>
         </main>
       </DefaultTemplate>
