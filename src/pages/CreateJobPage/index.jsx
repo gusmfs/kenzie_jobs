d5840a8449fc15798a338a9bbc8b6d53a6602d39
Alana Maia <alanaalvesmaia@gmail.com>
2023-08-31 14:19:30 -0300

    fix: import bugs on CreateJobPage

--- src/pages/CreateJobPage/index.jsx
+++ src/pages/CreateJobPage/index.jsx
@@ -1,3 +1,29 @@
+import { AiOutlinePlusCircle } from "react-icons/ai";
+import { Form } from "../../components/Form";
+import { Input } from "../../components/Inputs";
+import { TextArea } from "../../components/TextArea";
+import { DefaultTemplate } from "../DefaultTemplate";
+import styles from "./style.module.scss";
+
 export const CreateJobPage = () => {
-  return <></>;
+  return (
+    <>
+      <DefaultTemplate>
+        <main className="container sm">
+          <div className={styles.registerBox}>
+            <h2 className="title one blue center">Cadastre-se</h2>
+            <Form>
+              <Input type="text" placeholder="Cargo" />
+              <Input type="text" placeholder="Salário (opcional)" />
+              <TextArea placeholder="Descrição" />
+              <button>
+                <AiOutlinePlusCircle />
+                <span>Criar Vaga</span>
+              </button>
+            </Form>
+          </div>
+        </main>
+      </DefaultTemplate>
+    </>
+  );
 };
