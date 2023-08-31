b457a57719373741c69ae52960cfc26f0f2ac64c
Alana Maia <alanaalvesmaia@gmail.com>
2023-08-31 13:32:20 -0300

    refactor: structure RegisterPage

--- src/pages/RegisterPage/index.jsx
+++ src/pages/RegisterPage/index.jsx
@@ -1,11 +1,25 @@
+import { Form } from "../../components/Form";
+import { Input } from "../../components/Inputs";
 import { DefaultTemplate } from "../DefaultTemplate";
+import { AiOutlinePlusCircle } from "react-icons/ai";
 
 export const RegisterPage = () => {
   return (
     <>
       <DefaultTemplate>
         <main>
-          <h2>Cadastre-se</h2>
+          <div>
+            <h2>Cadastre-se</h2>
+            <Form>
+              <Input type="text" placeholder="Cargo" />
+              <Input type="text" placeholder="Salário (opcional)" />
+              <textarea name="" id="" cols="30" rows="10"></textarea>
+              <button>
+                <AiOutlinePlusCircle />
+                <span>Criar Vaga</span>
+              </button>
+            </Form>
+          </div>
         </main>
       </DefaultTemplate>
     </>
