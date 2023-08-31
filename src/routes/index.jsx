73e99dd707eb36056078a15940146bdf933c04d6
Rhayanny rodrigues <rhayannyrodrigues71 @gmail.com>
2023-08-31 16:06:58 -0300

    feat:LoginPage

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import { HomePage } from "../pages/HomePage";
 import { SearchPage } from "../pages/SearchPage";
+import { LoginPage } from "../pages/LoginPage";
 
 export const RoutesMain = () => {
   return (
     <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/search" element={<SearchPage />} />
-        {/* <Routes parth="/login" element={}/> */}
+      <Route path="/" element={<HomePage />} />
+      <Route path="/search" element={<SearchPage />} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
 };
