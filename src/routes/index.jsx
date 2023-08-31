09603c0d2f4050defeeaa19eb14a6a183c2fee4e
Alana Maia <alanaalvesmaia@gmail.com>
2023-08-31 14:15:41 -0300

    feat: create CreateJobPage and its route

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { HomePage } from "../pages/HomePage";
 import { SearchPage } from "../pages/SearchPage";
 import { RegisterPage } from "../pages/RegisterPage";
+import { CreateJobPage } from "../pages/CreateJobPage";
 
 export const RoutesMain = () => {
   return (
@@ -9,6 +10,8 @@ export const RoutesMain = () => {
       <Route path="/" element={<HomePage />} />
       <Route path="/search" element={<SearchPage />} />
       <Route path="/register" element={<RegisterPage />} />
+
+      <Route path="/create-job" element={<CreateJobPage />} />
     </Routes>
   );
 };
