c59b6ea251bcc3203126ba91fca85f03852e07b5
Iara Assis <iaraassisnega@gmail.com>
2023-08-31 16:57:34 -0300

    fix: routes dash

--- src/pages/DashboardPage/index.jsx
+++ src/pages/DashboardPage/index.jsx
@@ -11,7 +11,7 @@ export const DashboardPage = () => {
             <main className="container">
                 <section className={styles.dashboardContainer}>
                     <h1 className="title one">Kenzie Academy Brasil</h1>
-                    <h2 className="paragraph">Seja bem vindo (a), selecione uma das opções abaixo:</h2>
+                    <p className="paragraph">Seja bem vindo (a), selecione uma das opções abaixo:</p>
                     <div className={styles.dashButtons}>
                         <button className="title boldAlert">Minhas vagas</button>
 
