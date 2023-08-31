d95d50cbf270e436d60b96abead2bdad6da7aef4
Iara Assis <iaraassisnega@gmail.com>
2023-08-31 17:00:45 -0300

    fix: routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,10 @@
 import { Routes, Route } from "react-router-dom";
-import { HomePage } from "../pages/HomePage";
 import { SearchPage } from "../pages/SearchPage";
 import { LoginPage } from "../pages/LoginPage";
 import { RegisterPage } from "../pages/RegisterPage";
 import { CreateJobPage } from "../pages/CreateJobPage";
 import { DashboardPage } from "../pages/DashboardPage";
+import { HomePage } from "../pages/HomePage";
 
 
 
