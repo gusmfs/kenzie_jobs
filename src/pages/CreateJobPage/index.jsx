0b5987e5573828632a2471e058fa99509277e41f
Alana Maia <alanaalvesmaia@gmail.com>
2023-08-31 16:03:32 -0300

    refactor: CreateJobPage structure and style

--- src/pages/CreateJobPage/index.jsx
+++ src/pages/CreateJobPage/index.jsx
@@ -4,23 +4,33 @@ import { Input } from "../../components/Inputs";
 import { TextArea } from "../../components/TextArea";
 import { DefaultTemplate } from "../DefaultTemplate";
 import styles from "./style.module.scss";
+import { Link } from "react-router-dom";
+import { BiArrowBack } from "react-icons/bi";
 
 export const CreateJobPage = () => {
   return (
     <>
       <DefaultTemplate>
-        <main className="container sm">
+        <main className="container">
           <div className={styles.registerBox}>
-            <h2 className="title one blue center">Criar vaga</h2>
-            <Form>
-              <Input type="text" placeholder="Cargo" />
-              <Input type="text" placeholder="Salário (opcional)" />
-              <TextArea placeholder="Descrição" />
-              <button className={`${styles.buttonCreate} btnSolid`}>
-                <AiOutlinePlusCircle />
-                <span>Criar Vaga</span>
-              </button>
-            </Form>
+            <Link className={styles.link}>
+              <BiArrowBack />
+              <span class="paragraph bold">voltar</span>
+            </Link>
+            <div className="container xs">
+              <div className={styles.registerForm}>
+                <h2 className="title one blue center">Criar vaga</h2>
+                <Form>
+                  <Input type="text" placeholder="Cargo" />
+                  <Input type="text" placeholder="Salário (opcional)" />
+                  <TextArea placeholder="Descrição" />
+                  <button className={`${styles.buttonCreate} btnSolid`}>
+                    <AiOutlinePlusCircle />
+                    <span>Criar Vaga</span>
+                  </button>
+                </Form>
+              </div>
+            </div>
           </div>
         </main>
       </DefaultTemplate>
