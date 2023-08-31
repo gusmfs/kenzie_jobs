34822c732de82e58b5b83abcdf13d80fba5679a6
Iara Assis <iaraassisnega@gmail.com>
2023-08-31 09:53:51 -0300

    refactor: envolve search page main on defaultTemplate

--- src/pages/SearchPage/index.jsx
+++ src/pages/SearchPage/index.jsx
@@ -1,33 +1,40 @@
 import {MdSearch} from "react-icons/md";
 import styles from "./style.module.scss";
 import { useState } from "react";
+import { DefaultContext } from "react-icons/lib/esm";
+import { DefaultTemplate } from "../DefaultTemplate";
+import { JobsList } from "../../components/JobsList";
 
 export const SearchPage = () =>{
     // const [searchFiltered, setSearchFiltered] = useState(false);
 
     return(
-        <main className="container">
-            <div className={styles.searchContainer}>
-                <h1 className="title one">Busca de vagas</h1>
-                <p className="title boldAlert ">Digite o que você está procurando:</p>
-                <form>
-                    <input type="text" placeholder="Pesquisa"/>
-                    <button className="" type="submit">
-                        <MdSearch size={25}/>
-                    </button>
-                </form>
-                <div className={styles.filter}>
-                    <h2 className="paragraph strong">Resultados de busca para: <strong className="title boldAlert">desenvolvedor</strong></h2>
-                    <h3 className="title two ">Desculpe :(!</h3>
-                    <p className="paragraph">Nenhum resultado encontrado</p>
-
-                    <ul>
-                    </ul>
+        <DefaultTemplate>
+            <main className="container">
+                <div className={styles.searchContainer}>
+                    <h1 className="title one">Busca de vagas</h1>
+                    <p className="title boldAlert ">Digite o que você está procurando:</p>
+                    <form>
+                        <input type="text" placeholder="Pesquisa"/>
+                        <button className="" type="submit">
+                            <MdSearch size={25}/>
+                        </button>
+                    </form>
+                    <div className={styles.filter}>
+                        <h2 className="paragraph strong">Resultados de busca para: <strong className="title boldAlert">desenvolvedor</strong></h2>
+                        <h3 className="title two ">Desculpe :(!</h3>
+                        <p className="paragraph">Nenhum resultado encontrado</p>
+    
+                        <>
+                            <JobsList/>
+                        </>
+                    </div>
                 </div>
-            </div>
-        </main>
-    )
-}
+            </main>
+        </DefaultTemplate>
+    );
+
+};
 
 
 
