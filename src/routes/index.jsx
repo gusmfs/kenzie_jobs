7f392deb6b60adbe6b0978ec0769f9e52586cc58
Iara Assis <iaraassisnega@gmail.com>
2023-08-31 16:09:47 -0300

    Feat: create DashboardPage

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { HomePage } from "../pages/HomePage";
 import { SearchPage } from "../pages/SearchPage";
 import { RegisterPage } from "../pages/RegisterPage";
+import { DashboardPage } from "../pages/DashboardPage";
 
 export const RoutesMain = () => {
   return (
@@ -9,6 +10,7 @@ export const RoutesMain = () => {
       <Route path="/" element={<HomePage />} />
       <Route path="/search" element={<SearchPage />} />
       <Route path="/register" element={<RegisterPage />} />
+      <Route path="/dashboard" element={<DashboardPage/>} />
     </Routes>
   );
 };
