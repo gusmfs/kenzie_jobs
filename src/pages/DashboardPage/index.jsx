fcd82136dc30a733229b232f188fe8cd4ac01caa
Iara Assis <iaraassisnega@gmail.com>
2023-09-01 14:30:44 -0300

    fix: update dashboardpage

--- src/pages/DashboardPage/index.jsx
+++ src/pages/DashboardPage/index.jsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import { DefaultTemplate } from "../DefaultTemplate";
 import styles from "./style.module.scss";
-import { MdOutlineDelete, MdOutlineEdit } from "react-icons/md";
 import {  AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 import { MyJobsSection } from "../../components/sections/MyJobsSection";
 
@@ -18,37 +17,16 @@ export const DashboardPage = () => {
                         <button className="title boldAlert">Minhas candidaturas</button>
                         <ul>
                             <MyJobsSection/>
+                            <MyJobsApplicantions/>
                         </ul>
-
                     </div>
                 </section>
-
-                <button className="title boldAlert">Minhas candidaturas</button>
-                    <ul>
-                        <li>
-                            <div>
-                                <h2>José da Silva - Desenvolvedor Full Stack Jr</h2>
-                                <button> 
-                                    <AiOutlineMinus size={21} />
-                                </button>
-                            </div>
-                            <p>Detalhes da candidatura: 
-                                E-mail:josedasilva@email.com
-                            </p>
-                        </li>
-                        <li>
-                            <div>
-                                <h2>Otavio Silva - Desenvolvedor Full Stack Jr</h2>
-                            </div>
-                            <button>
-                                <AiOutlinePlus size={21} />
-                            </button>
-                        </li>
-                    </ul>
             </main>
         </DefaultTemplate>
     );
 };
+
+
 
 
 
