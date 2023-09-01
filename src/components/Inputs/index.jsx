6f6189bab7b9f27a0da8070dc804398f4a729567
Alana Maia <alanaalvesmaia@gmail.com>
2023-09-01 15:51:48 -0300

    feat: ApplyJobModal register inputs

--- src/components/Inputs/index.jsx
+++ src/components/Inputs/index.jsx
@@ -4,7 +4,7 @@ import styles from "./style.module.scss";
 export const Input = forwardRef(({ error, required, ...rest }, ref) => {
   return (
     <div className={styles.inputContainer}>
-      <input className="inputForm" ref={ref} {...rest} />
+      <input className="inputForm" ref={ref} required={required} {...rest} />
       {error ? <p className="label red">{error.message}</p> : null}
     </div>
   );
