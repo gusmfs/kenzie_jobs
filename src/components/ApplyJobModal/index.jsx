9b0e3ddc14bc79f2ac8a1f9e87c2b50b31d5210c
Rhayanny rodrigues <rhayannyrodrigues71 @gmail.com>
2023-09-01 16:32:32 -0300

    refactor: style modal

--- src/components/ApplyJobModal/index.jsx
+++ src/components/ApplyJobModal/index.jsx
@@ -13,39 +13,41 @@ export const ApplyJobModal = ({ setIsOpen }) => {
 
   return (
     <aside className={style.modalOverlay} role="dialog">
-      <div>
-        <h2 className="title two">Candidatar-se</h2>
-        <span onClick={() => setIsOpen(false)}>
-          <AiOutlineClose />
-        </span>
-      </div>
-      <p className="paragraph">
-        Você está se canditando para
-        <strong> Desenvolvedor Full Stack Jr</strong> em
-        <strong> Kenzie Academy Brasil</strong>
-      </p>
-      <Form handleSubmit={handleSubmit} submit={submit}>
-        <Input
-          type="text"
-          placeholder="Nome"
-          required={true}
-          {...register("name")}
-        />
-        <Input
-          type="email"
-          placeholder="E-mail"
-          required={true}
-          {...register("email")}
-        />
-        <Input
-          type="text"
-          placeholder="Linkedin"
-          required={true}
-          {...register("linkedin")}
-        />
+      <div className={style.Modal}>
+        <div>
+          <h2 className="title two">Candidatar-se</h2>
+          <span onClick={() => setIsOpen(false)}>
+            <AiOutlineClose />
+          </span>
+        </div>
+        <p className="paragraph">
+          Você está se canditando para
+          <strong> Desenvolvedor Full Stack Jr</strong> em
+          <strong> Kenzie Academy Brasil</strong>
+        </p>
+        <Form handleSubmit={handleSubmit} submit={submit}>
+          <Input
+            type="text"
+            placeholder="Nome"
+            required={true}
+            {...register("name")}
+          />
+          <Input
+            type="email"
+            placeholder="E-mail"
+            required={true}
+            {...register("email")}
+          />
+          <Input
+            type="text"
+            placeholder="Linkedin"
+            required={true}
+            {...register("linkedin")}
+          />
 
-        <button className="btnSolid">Candidatar-se</button>
-      </Form>
+          <button className="btnSolid">Candidatar-se</button>
+        </Form>
+      </div>
     </aside>
   );
 };
