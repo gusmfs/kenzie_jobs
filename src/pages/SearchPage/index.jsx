e62dcaa910fc0b510ad204744b0cb656df8d09c8
Iara Assis <iaraassisnega@gmail.com>
2023-09-02 11:23:40 -0300

    feat: formSearchPage

--- src/pages/SearchPage/index.jsx
+++ src/pages/SearchPage/index.jsx
@@ -9,7 +9,7 @@ import { Form } from "react-router-dom";
 
 export const SearchPage = () =>{
     const [searchTerm, setSearchTerm] = useState("");
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, formState: {errors}, } = useForm();
 
     const submit = (formData) => {
         console.log(formData);
@@ -24,15 +24,16 @@ export const SearchPage = () =>{
                 <div className={styles.searchContainer}>
                     <h1 className="title one">Busca de vagas</h1>
                     <p className="title boldAlert ">Digite o que você está procurando:</p>
-                    <Form>
+                    <form onSubmit={handleSubmit(submit)} >
                         <Input type="text" 
                         placeholder="Pesquisa"
-                        value={searchTerm}
+                        {...register("position")}
+                        error={errors.position}
                         />
                         <button className={styles.buttonSearch} type="submit">
                             <MdSearch size={25}/>
                         </button>
-                    </Form>
+                    </form>
                     <div className={styles.filter}>
                         {/* <h2 className="paragraph strong">Resultados de busca para: <strong className="title boldAlert">{searchTerm}</strong></h2>
                         {filteredJobs.length === 0 ? (
