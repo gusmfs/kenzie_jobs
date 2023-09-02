f5a9781d082ef1ca7be7df6bbc0d62b121963ce7
Alana Maia <alanaalvesmaia@gmail.com>
2023-09-02 15:44:21 -0300

    refactor: JobsList

--- src/components/JobsList/index.jsx
+++ src/components/JobsList/index.jsx
@@ -2,18 +2,17 @@ import { useState } from "react";
 import { JobsListCard } from "./JobsListCard";
 import styles from "./style.module.scss";
 import { ApplyJobModal } from "../ApplyJobModal";
-import { useContext } from "react";
-import { JobContext } from "../../providers/jobContext";
+// import { useContext } from "react";
+// import { JobContext } from "../../providers/jobContext";
 
-export const JobsList = () => {
+export const JobsList = ({ list }) => {
   const [isOpen, setIsOpen] = useState(false);
-  const { jobs } = useContext(JobContext);
   return (
     <>
       {isOpen ? <ApplyJobModal setIsOpen={setIsOpen} /> : null}
       <ul className={styles.flexBox}>
-        {jobs.length > 0 &&
-          jobs.map((job) => (
+        {list.length > 0 &&
+          list.map((job) => (
             <li key={job.id}>
               <JobsListCard job={job} setIsOpen={setIsOpen} />
             </li>
