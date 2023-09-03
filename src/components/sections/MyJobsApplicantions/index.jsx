fd37fe391394a6daf30dd7a6a335ae4a73a81429
Gustavo Medeiros <oigostavo@gmail.com>
2023-09-03 17:07:27 -0300

    .

--- src/components/sections/MyJobsApplicantions/index.jsx
+++ src/components/sections/MyJobsApplicantions/index.jsx
@@ -1,24 +1,12 @@
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+import { MyApplicantionsList } from "../../MyApplicantionsList";
 import styles from "./style.module.scss";
 
 export const MyJobsApplicantions = () => {
     return(
         <section>
             <h2 className="title one">Minhas Candidaturas</h2>
-                {/* <li className={styles.containerLiMinus}>
-                    <div>
-                        <h3 className="title tree">José da Silva - Desenvolvedor Full Stack Jr</h3>
-                        <button> <AiOutlineMinus size={21} /></button>
-                    </div>
-                    <span>
-                        <p className="paragraph">Detalhes da candidatura:</p>
-                        <p> E-mail: <strong className="title boldAlert">josedasilva@email.com</strong></p>
-                    </span>
-                </li>
-                <li className={styles.containerLiPlus}>
-                    <p className="title tree">Otavio Silva - Desenvolvedor Full Stack Jr</p>
-                    <button><AiOutlinePlus size={21} /></button>
-                </li> */}
+            <MyApplicantionsList/>
         </section>
     );
 };
