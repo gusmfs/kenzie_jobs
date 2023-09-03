fff393fe8a51988d5c2fb7686e1bf8f6b6a59bd6
Alana Maia <alanaalvesmaia@gmail.com>
2023-09-03 13:57:00 -0300

    feat: create MyJobsList and MyJobsListCard

---
+++ src/components/MyJobsList/index.jsx
@@ -0,0 +1,3 @@
+export const MyJobList = () => {
+  return <ul></ul>;
+};
--- src/components/sections/MyJobsSection/index.jsx
+++ src/components/sections/MyJobsSection/index.jsx
@@ -3,46 +3,42 @@ import { MdOutlineDelete, MdOutlineEdit } from "react-icons/md";
 import styles from "./style.module.scss";
 
 export const MyJobsSection = () => {
-    return (
-        <section >
-            <div className={styles.containerJobs}>
-                <h2 className="title one">Minhas vagas</h2>
-                <button className="btnSolid">
-                        <AiOutlinePlus />
-                    Criar vaga
-                </button>
-            </div>
-            <li className={styles.containerLi}>
-                <div>
-                    <h3 className="title tree">Desenvolvedor Full Stack Jr</h3>
-                </div>
-                <span>
-                    <button title="Editar" aria-label="edit">
-                        <MdOutlineEdit size={25} />
-                    </button>
-                    <button title="Remover" aria-label="remove">
-                        <MdOutlineDelete size={25} />
-                    </button>
-                </span>
-            </li>
-            <li className={styles.containerLi}>
-                <div>
-                    <h3 className="title tree">Desenvolvedor Full Stack Jr</h3>
-                </div>
-                <span>
-                    <button title="Editar" aria-label="edit">
-                        <MdOutlineEdit size={25} />
-                    </button>
-                    <button title="Remover" aria-label="remove">
-                        <MdOutlineDelete size={25} />
-                    </button>
-                </span>
-            </li>
-        </section>
-    );
+  return (
+    <section>
+      <div className={styles.containerJobs}>
+        <h2 className="title one">Minhas vagas</h2>
+        <button className="btnSolid">
+          <AiOutlinePlus />
+          Criar vaga
+        </button>
+      </div>
+      
+      {/* <li className={styles.containerLi}>
+        <div>
+          <h3 className="title tree">Desenvolvedor Full Stack Jr</h3>
+        </div>
+        <span>
+          <button title="Editar" aria-label="edit">
+            <MdOutlineEdit size={25} />
+          </button>
+          <button title="Remover" aria-label="remove">
+            <MdOutlineDelete size={25} />
+          </button>
+        </span>
+      </li>
+      <li className={styles.containerLi}>
+        <div>
+          <h3 className="title tree">Desenvolvedor Full Stack Jr</h3>
+        </div>
+        <span>
+          <button title="Editar" aria-label="edit">
+            <MdOutlineEdit size={25} />
+          </button>
+          <button title="Remover" aria-label="remove">
+            <MdOutlineDelete size={25} />
+          </button>
+        </span>
+      </li> */}
+    </section>
+  );
 };
-
-
-
-
-
