c1ea1c5d597d63dab8341e12d020bcffea4674a9
Iara Assis <iaraassisnega@gmail.com>
2023-09-03 16:22:25 -0300

    refactor My ApplicantionsList

---
+++ src/components/MyApplicantionsList/MyApplicantionsCard/index.jsx
@@ -0,0 +1,40 @@
+import { useContext } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+import { CompanyContext } from "../../../providers/companyContext";
+import styles from "./style.module.scss"
+
+export const MyApplicantionsCard = ({job}) => {
+    // [
+    //     {
+    //         "id": 1,
+    //         "jobId": 1,
+    //         "userId": 1,
+    //         "name": "Tsunode",
+    //         "email": "tsunode@mail.com",
+    //         "linkedin": "https://www.linkedin.com/in/tsunode"
+    //     }
+    // ]
+
+    const { } = useContext(CompanyContext);
+
+
+    return(
+        <>
+            <li className={styles.containerLiMinus}>
+                <div>
+                    <h3 className="title tree">José da Silva - Desenvolvedor Full Stack Jr</h3>
+                    <button> <AiOutlineMinus size={21} /></button>
+                </div>
+                <span>
+                    <p className="paragraph">Detalhes da candidatura:</p>
+                    <p> E-mail: <strong className="title boldAlert">josedasilva@email.com</strong></p>
+                </span>
+                </li>
+                <li className={styles.containerLiPlus}>
+                    <p className="title tree">Otavio Silva - Desenvolvedor Full Stack Jr</p>
+                    <button><AiOutlinePlus size={21} /></button>
+                </li>
+        
+        </>
+    );
+};
--- src/components/MyApplicantionsList/index.jsx
+++ src/components/MyApplicantionsList/index.jsx
@@ -1,7 +1,13 @@
+import { MyApplicantionsCard } from "./MyApplicantionsCard";
+
 export const MyApplicantionsList = () => {
+
     return(
         <ul>
-            
+            {/* fazer map */}
+            <MyApplicantionsCard/>
         </ul>
     );
+
+
 };
